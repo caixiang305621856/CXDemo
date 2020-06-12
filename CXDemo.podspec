643f6d4379fe905395bf3336b0b4ab91da161e28
caixiang <yanyan305621856@sina.com>
2020-06-12 09:19:59 +0800

    demo init

--- CXDemo.podspec
+++ CXDemo.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'CXDemo'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of CXDemo.'
+  s.summary          = 'CXDemo.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -17,9 +17,7 @@ Pod::Spec.new do |s|
 #   * Write the description between the DESC delimiters below.
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
-  s.description      = <<-DESC
-TODO: Add long description of the pod here.
-                       DESC
+  s.description      = 'TestTestTest'
 
   s.homepage         = 'https://github.com/caixiang305621856/CXDemo'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
@@ -28,15 +26,17 @@ TODO: Add long description of the pod here.
   s.source           = { :git => 'https://github.com/caixiang305621856/CXDemo.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '8.0'
+  s.ios.deployment_target = '9.0'
 
   s.source_files = 'CXDemo/Classes/**/*'
   
   # s.resource_bundles = {
   #   'CXDemo' => ['CXDemo/Assets/*.png']
   # }
-
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+  # s.preserve_paths = '*.pch'
+  # s.prefix_header_file = 'Graver.pch'
+  # s.requires_arc = true
+  # s.dependency 'SDWebImage', '~>4.2'
 end
--- Example/CXDemo/CXViewController.m
+++ Example/CXDemo/CXViewController.m
@@ -8,22 +8,56 @@
 
 #import "CXViewController.h"
 
-@interface CXViewController ()
+@interface CXViewController ()<UITableViewDelegate,UITableViewDataSource>
+{
+    NSArray *_data;
+}
+@property (strong, nonatomic) UITableView *tableview;
 
 @end
 
 @implementation CXViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.tableview];
+    _data = @[@"路由",@"Block",@"Category",@"KVO",@"NSObject",@"runtime"];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UITableViewDataSource & UITableViewDelegate
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellId = @"cellId";
+    NSInteger row = indexPath.row;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    NSString *str = _data[row];
+    cell.textLabel.text = str;
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 60.f;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _data.count;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - set&get
+- (UITableView *)tableview {
+    if (!_tableview) {
+        _tableview = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableview.delegate = self;
+        _tableview.dataSource = self;
+        _tableview.tableFooterView = [UIView new];
+    }
+    return _tableview;
 }
 
 @end
